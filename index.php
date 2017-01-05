3cd317b97c7669fe1a172ecb72eac86d726dc95d
shern91 <yschin_0614@hotmail.com>
2017-01-05 16:59:47 +0800

    styling the index.php

--- index.php
+++ index.php
@@ -7,9 +7,18 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <style type="text/css">
+      body{
+        padding-top: 40px;
+      }
+    </style>
   </head>
   <body>
     <div class="container">
+      <div class="jumbotron">
+        <h1>JavaScript30 Demo</h1>
+        <p>I will upload once a done a tutorial. Thank for visit!</p>
+        </div>
       <div class="table-responsive">
         <table class="table">
           <tr>
@@ -18,7 +27,39 @@
           </tr>
           <tr>
             <td>1</td>
-            <td>JavaScript Drum Kit</td>
+            <td><a href="./video2/">JavaScript Drum Kit</a></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td><a href="./video3/">CSS + JS Clock</a></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td><a href="#">Playing with CSS Variables and JS</a></td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td><a href="#">Array Cardio Day 1</a></td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td><a href="#">Flex Panels Image Gallery</a></td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td><a href="#">Ajax Type Ahead</a></td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td><a href="#">Array Cardio Day 2</a></td>
+          </tr>
+          <tr>
+            <td>8</td>
+            <td><a href="#">Fun with HTML5 Canvas</a></td>
+          </tr>
+          <tr>
+            <td>9</td>
+            <td><a href="#">4 Must Know Dev Tools Tricks</a></td>
           </tr>
         </table>
       </div>
