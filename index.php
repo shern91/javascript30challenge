f26b4b997b20ecbf1e2f9430280165588e898e7c
shern91 <yschin_0614@hotmail.com>
2017-01-05 16:48:10 +0800

    Add index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>JavaScript30Challenge Index</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <div class="table-responsive">
+        <table class="table">
+          <tr>
+          <th width="20%">No.</th>
+          <th width="80%">Description for Tutorial</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>JavaScript Drum Kit</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </body>
+</html>
