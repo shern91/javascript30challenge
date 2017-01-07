9cf56394deae9e65d53340db7e7e6fe95bc47a45
shern91 <yschin_0614@hotmail.com>
2017-01-07 20:33:54 +0800

    make some change in index.php

--- index.php
+++ index.php
@@ -19,65 +19,65 @@
         <h1>JavaScript30 Demo</h1>
         <p>I will upload once a done a tutorial. Thank for visit!</p>
         </div>
-      <div class="table-responsive">
-        <table class="table">
+      <div class="table">
+        <table class="table table-hover">
           <thead>
           <tr>
-            <th width="20%">No.</th>
-            <th width="60%">Description for Tutorial</th>
-            <th width="20%">Action</th>
+            <th>No.</th>
+            <th>Description for Tutorial</th>
+            <th>Action</th>
           </tr>
           </thead>
           <tbody>
           <tr>
             <td>1</td>
-            <td><a href="./video1/">JavaScript Drum Kit</a></td>
-            <td></td>
+            <td>JavaScript Drum Kit</td>
+            <td><a class="btn btn-primary btn-xs btn" href="./video1/" target="_blank" role="button">View</a></td>
           </tr>
           <tr>
             <td>2</td>
-            <td><a href="./video2/">CSS + JS Clock</a></td>
-            <td></td>
+            <td>CSS + JS Clock</td>
+            <td><a class="btn btn-primary btn-xs btn" href="./video2/" target="_blank" role="button">View</a></td>
           </tr>
           <tr>
             <td>3</td>
-            <td><a href="./video3/">Playing with CSS Variables and JS</a></td>
-            <td></td>
+            <td>Playing with CSS Variables and JS</td>
+            <td><a class="btn btn-primary btn-xs btn" href="./video3/" target="_blank" role="button">View</a></td>
           </tr>
           <tr>
             <td>4</td>
-            <td><a href="./video4/">Array Cardio Day 1</a></td>
-            <td></td>
+            <td>Array Cardio Day 1</td>
+            <td><a class="btn btn-primary btn-xs btn" href="./video4/" target="_blank" role="button">View</a></td>
           </tr>
           <tr>
             <td>5</td>
-            <td><a href="./video5/">Flex Panels Image Gallery</a></td>
-            <td></td>
+            <td>Flex Panels Image Gallery</td>
+            <td><a class="btn btn-primary btn-xs btn" href="./video5/" target="_blank" role="button">View</a></td>
           </tr>
           <tr>
             <td>6</td>
-            <td><a href="./video6/">Ajax Type Ahead</a></td>
-            <td></td>
+            <td>Ajax Type Ahead</td>
+            <td><a class="btn btn-primary btn-xs btn" href="./video6/" target="_blank" role="button">View</a></td>
           </tr>
           <tr>
             <td>7</td>
-            <td><a href="./video7/">Array Cardio Day 2</a></td>
-            <td></td>
+            <td>Array Cardio Day </td>
+            <td><a class="btn btn-primary btn-xs btn" href="./video7/" target="_blank" role="button">View</a></td>
           </tr>
           <tr>
             <td>8</td>
-            <td><a href="./video8/">Fun with HTML5 Canvas</a></td>
-            <td></td>
+            <td>Fun with HTML5 Canvas</td>
+            <td><a class="btn btn-primary btn-xs btn" href="./video8/" target="_blank" role="button">View</a></td>
           </tr>
           <tr>
             <td>9</td>
-            <td><a href="./video9/">4 Must Know Dev Tools Tricks</a></td>
-            <td></td>
+            <td>4 Must Know Dev Tools Tricks</td>
+            <td><a class="btn btn-primary btn-xs btn" href="./video9/" target="_blank" role="button">View</a></td>
           </tr>
           <tr>
             <td>10</td>
-            <td><a href="./video10/">Hold Shift to Check Multiple Checkboxes</a></td>
-            <td></td>
+            <td>Hold Shift to Check Multiple Checkboxes</td>
+            <td><a class="btn btn-primary btn-xs btn" href="./video10/" target="_blank" role="button">View</a></td>
           </tr>
           </tbody>
         </table>
